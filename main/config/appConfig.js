2decf88ec6766a7093255131e981e8c6b8ef3b8b
ByRUDAN <rudan@example.com>
2026-08-18 17:01:22 +0800

    BS127: 版本升至 0.9.4-BS127

--- main/config/appConfig.js
+++ main/config/appConfig.js
@@ -4,7 +4,7 @@ module.exports = {
     nameEn: "OOOInterface Update Assistant",
     shortName: "OUA",
     version: "0.9.4",
-    fullVersion: "0.9.４-BS126",
+    fullVersion: "0.9.４-BS127",
     copyright: "© 2026 ByRUDAN 保留所有权利。",
     contact: "wyjcrtu@proton.me"
   },
